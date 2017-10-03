52116232853f7821951f6cafee8bc3b2f516e20b
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-03 13:37:31 -0700

    Bug fix: opening multiple filter folders then clicking on a single filter works now.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ What problem or need does your project solve?
 
 - Online shopping sites sometimes have problems with filtering. For example, on all Nordstrom category pages, four filters are shown: Size, Color, Price, and Brand. However, for many categories of item, these categories are not relevant or not informative. For example, when I am shopping in the Home category, the Size filter usually only has "OS" (one size) which is pretty useless if I'm looking for a throw pillow or an accent chair. What if these filters were populated dynamically, directly from the JSON file containing the products to be displayed on the page? This is a tool that will read in a JSON file of products, validate it against a JSONSchema (either provided by the user or the included boilerplate version), and dynamically append these filter categories to a page.
 
-Additionally, I will use local storage to retain a user's filter information. Currently, when you move on to a different category of item, that size is not retained when you go back to it. For example, if I search for women's jeans for myself, then go look for men's jeans for my partner, then go back to women's jeans, I would like the size filter to persist within a single session and across sessions. A user can remove this information easily by un-checking the filter. (This info could also be sent back to the server & stored for better user behavior tracking! I.e. if there is a closeout event and my favorite brand/size of jeans is included, I would love to have an email notification. )
+Additionally, I will use local storage to retain a user's filter information. Currently, when you move on to a different category of item, that size is not retained when you go back to it. For example, if I search for women's jeans for myself, then go look for men's jeans for my partner, then go back to women's jeans, I would like the size filter to persist within a single session and across sessions. A user can remove this information easily by un-checking the filter. (This info could also be sent back to the server & stored for better user behavior tracking! I.e. if there is a closeout event and my favorite brand/size of jeans is included, I would love to have an email notification.)
 
 ## Installation ##
 
@@ -26,3 +26,6 @@ Additionally, use of this library requires a bundler - the example is running We
 const DynamicFilter = require('dynamic-filters')
 const df = new DynamicFilter(productSchema, data)
 ```
+productSchema is a [JSON-schema](http://jsonschema.org) describing the structure of your data. A default option is included, which has ID, name, and price as required fields and an array of optional fields.
+
+data is a JSON object with a single key-value pair, where the key is "products" and the value is an array of product objects.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const data = require ('../data/test-products-2.json')
 const products = data.products
 const productSchema = require('../model/product.schema.json')
-
 const DynamicFilter = require('../model')
+
 const df = new DynamicFilter(productSchema, data, null, 5)
 
 const filterObject = df.getFilters()
@@ -16,10 +16,11 @@ const productDiv = document.getElementById('products')
 const filtersList = document.createElement('ul')
 filtersList.className = "list-group" // bootstrap
 
-const activeFilterDisplay = document.getElementById('activeFilterDisplay')
 
+// Active Filter Display - holds the name of the currently active filter.
+const activeFilterDisplay = document.getElementById('activeFilterDisplay')
 // would be nice to eventually have dismissable pills with each filter on it
-// but for now, it's just one clear button
+// but for now, it's just one clear button.
 const clearButton = document.getElementById('clearButton')
 clearButton.addEventListener('click', e => {
   filters.filteredProducts = []
@@ -32,38 +33,54 @@ clearButton.addEventListener('click', e => {
 // Called on click of filter title
 // Generates UL & LIs for the filter details & appends to FilterDetailDiv
 
-function displayFilterDetails(filterDetail, parentDiv) {
-
+function displayFilterDetails(filterName, filterDetail, parentDiv) {
+  // only display filter details if not already displayedd.
   if(parentDiv.childNodes.length <= 1) {
-    const itemList = document.createElement('UL') // create a new UL
+    // Create a new list with class name "list-group"
+    const itemList = document.createElement('UL')
     itemList.className = "list-group"
-    filterDetail.forEach(detailedFilter => { // for each filter detail
-      const listItem = document.createElement('LI') // create a new LI
+
+    // filterDetail is an array of values to be displayed.
+    filterDetail.forEach(detailedFilter => {
+      // create a new LI and give it class names "list-group-item" and "detail-item"
+      const listItem = document.createElement('LI')
       listItem.className = "list-group-item detail-item"
+      // if it is the price, we only want to display the "label" property, not the object.
       if(filters.activeFilter === 'price') {
         listItem.textContent = detailedFilter.label
       }  else {
         listItem.textContent = detailedFilter // LI text content is name of detail
       }
 
-      listItem.addEventListener('click', e => { // Add event listener to detail item
+      // Add a click event listener that will actually do the filtering.
+      // Each list item gets the click handler.
+      listItem.addEventListener('click', e => {
+        filters.activeFilter = filterName
         activeFilterDisplay.textContent = `${filters.activeFilter}: ${detailedFilter}`
+        // filter the products and display the results
         filters.filteredProducts = products.filter(product => {
+          // 3 options for filtering.
+          // Price is filtered by "buckets", i.e. over X but under Y.
           if(filters.activeFilter === 'price') { // bucket filtering
             activeFilterDisplay.textContent = `${filters.activeFilter}: ${detailedFilter.label}`
             return  ((parseFloat(product.price) >  detailedFilter.bucket[0])
                   && (parseFloat(product.price) <= detailedFilter.bucket[1]))
-          } else if ((filters.activeFilter === 'tags') || (filters.activeFilter === 'keywords')) { // one-of-many filtering
+          } // Tags or keywords are "one-of-many" type, ie we want to filter for products
+            // that include the tag or keyword in an array.
+          else if ((filters.activeFilter === 'tags') || (filters.activeFilter === 'keywords')) {
             return product[filters.activeFilter].includes(detailedFilter)
-          } else { // exact-match filtering
+          } else { // Otherwise it is an exact-match filtering, so just check for strict equality.
             return (product[filters.activeFilter] === detailedFilter)
           }
         })
+        // Call to displayProducts clears out the current display and updates it to the filteredProducts.
         displayProducts(filters.filteredProducts)
       })
+      // End event listener.
+      // Append the item (filter detail) to the list.
       itemList.append(listItem)
     })
-
+    // Append the item List to the parent div.
     parentDiv.append(itemList)
   }
 }
@@ -73,18 +90,26 @@ function collapseFilterDetails(name, listItem) {
   listItem.classList.remove('detail-opened')
 }
 
+
+// Display Filter Names generates the top-level filter list (ie. what is shown before any clicks.)
 function displayFilterNames() {
+  // filters is the Object.keys from the filterObject.
   filters.forEach(name => {
+    // create an LI and give it "list-group-item" bootstrap class & content of name
     const listItem = document.createElement('LI')
     listItem.className = "list-group-item"
     listItem.textContent = name
 
+    // click event listener will display or collapse the filter details
     listItem.addEventListener('click', e => {
-      filters.activeFilter = name
+      // set activeFilter to name, so we know what we are filtering by
+
       if(e.target.className.includes("detail-opened")){
+        filters.activeFilter = ""
         collapseFilterDetails(name, listItem)
       } else {
-        displayFilterDetails(filterObject[name], listItem)
+        filters.activeFilter = name
+        displayFilterDetails(name, filterObject[name], listItem)
         e.target.classList.add("detail-opened")
       }
 
